4f92aceb5cdc44640d48dc63f3b9c36c537990e9
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2022-06-07 15:56:05 +0300

    Common katmanı oluşturuldu. Data taşıyan ve data taşımayan response yapıları kuruldu.

---
+++ Ozkky.AdvertisementApp.Common/IResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Ozkky.AdvertisementApp.Common.Response;
+
+namespace Ozkky.AdvertisementApp.Common
+{
+    public interface IResponse
+    {
+        string Message { get; set; }
+
+        ResponseType ResponseType { get; set; }
+    }
+}

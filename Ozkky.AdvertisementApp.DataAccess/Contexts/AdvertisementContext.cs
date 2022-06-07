089c6f2c027e1c198856a55ec5c5724ba78eefee
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2022-06-07 19:54:06 +0300

    Data katmanı oluşturuldu. Entityler ile DbSetler yazıldı.

---
+++ Ozkky.AdvertisementApp.DataAccess/Contexts/AdvertisementContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ozkky.AdvertisementApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ozkky.AdvertisementApp.DataAccess.Contexts
+{
+    public class AdvertisementContext: DbContext
+    {
+        public AdvertisementContext(DbContextOptions<AdvertisementContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Advertisement> Advertisements { get; set; }
+
+        public DbSet<AdvertisementAppUser> AdvertisementAppUsers { get; set; }
+
+        public DbSet<AdvertisementAppUserStatus> AdvertisementAppUserStatuses { get; set; }
+
+        public DbSet<AppRole> AppRoles { get; set; }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+
+        public DbSet<AppUserRole> AppUserRoles { get; set; }
+
+        public DbSet<Gender> Genders { get; set; }
+
+        public DbSet<MilitaryStatus> MilitaryStatuses { get; set; }
+
+        public DbSet<ProvidedService> ProvidedServices { get; set; }
+
+    }
+}

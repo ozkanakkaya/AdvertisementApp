a8e63fce63d09d8b110f0cdea701fa2ec6ed7567
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2022-06-08 19:33:09 +0300

    Extention metod ile Connection String (databese) bağlantısı yapıldı. Migration oluşturuldu.

---
+++ Ozkky.AdvertisementApp.Business/DependencyResolvers/Microsoft/DependencyExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ozkky.AdvertisementApp.DataAccess.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ozkky.AdvertisementApp.Business.DependencyResolvers.Microsoft
+{
+    public static class DependencyExtension
+    {
+        public static void AddDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AdvertisementContext>(opt =>
+            {
+                opt.UseSqlServer(configuration.GetConnectionString("Local"));
+            });
+        }
+    }
+}
--- Ozkky.AdvertisementApp.DataAccess/Configurations/AdvertisementAppUserConfiguration.cs
+++ Ozkky.AdvertisementApp.DataAccess/Configurations/AdvertisementAppUserConfiguration.cs
@@ -21,7 +21,7 @@ namespace Ozkky.AdvertisementApp.DataAccess.Configurations
 
             builder.Property(x => x.CvPath).HasMaxLength(500).IsRequired();
 
-            builder.HasOne(x => x.Advertisement).WithMany(x => x.AdvertisementAppUsers).HasForeignKey(x => x.Advertisement);
+            builder.HasOne(x => x.Advertisement).WithMany(x => x.AdvertisementAppUsers).HasForeignKey(x => x.AdvertisementId);
             builder.HasOne(x => x.AppUser).WithMany(x => x.AdvertisementAppUsers).HasForeignKey(x => x.AppUserId);
             builder.HasOne(x => x.AdvertisementAppUserStatus).WithMany(x => x.AdvertisementAppUsers).HasForeignKey(x => x.AdvertisementAppUserStatusId);
             builder.HasOne(x => x.MilitaryStatus).WithMany(x => x.AdvertisementAppUsers).HasForeignKey(x => x.MilitaryStatusId);

bbfa9e80ec79e9a4ee506ec00a91918edc131379
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2022-06-14 04:45:31 +0300

    Admin yetkisinde ilanların liste sayfası yapıldı. İlanlar listelendi. Application/List.cshtml

---
+++ Ozkky.AdvertisementApp.UI/Controllers/ApplicationController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Ozkky.AdvertisementApp.Business.Interfaces;
+using Ozkky.AdvertisementApp.UI.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ozkky.AdvertisementApp.UI.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class ApplicationController : Controller
+    {
+        private readonly IAdvertisementService _advertisementService;
+
+        public ApplicationController(IAdvertisementService advertisementService)
+        {
+            _advertisementService = advertisementService;
+        }
+
+        public async Task<IActionResult> List()
+        {
+            var response = await _advertisementService.GetAllAsync();
+            return this.ResponseView(response);
+        }
+    }
+}
